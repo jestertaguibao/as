51dcd2128d1d2f8e3625d0567f062575beae9566
jestertaguibao <taguibaojester@gmail.com>
2016-04-30 17:25:29 +0800

    update

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 <!-- SCRIPT HERE -->
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
