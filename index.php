2ef3a65cedc2d3fbad219ce913f714eba447eee2
jestertaguibao <taguibaojester@gmail.com>
2016-05-11 15:07:07 +0800

    update

--- index.php
+++ index.php
@@ -1,44 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Anime Saitama</title>
 <meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width">
+<!-- CSS HERE -->
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+
 <!-- SCRIPT HERE -->
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<!-- SCRIPT HERE -->
 
 </head>
 <body>
+		
+       
 
-		<ul data-role="listview" data-filter="true" data-filter-reveal="true" data-filter-placeholder="Search fruits..." data-inset="true">
 
-		<?php
-		include('db_connect.php');
-		$sql = "SELECT * FROM title ORDER BY title_name ASC";
-		$result = $conn->query($sql);
+<form>
+    <input data-type="search" id="filterControlgroup-input">
+</form>
+<div data-role="controlgroup" data-filter="true" data-filter-reveal="true" data-filter-placeholder="Search Anime title" data-input="#filterControlgroup-input">
+	<?php
+			include('db_connect.php');
+			$sql = "SELECT * FROM title ORDER BY title_name ASC";
+			$result = $conn->query($sql);
 
-		    if ($result->num_rows > 0) {
-		        // output data of each row
-		        while($row = $result->fetch_assoc()) {
+			    if ($result->num_rows > 0) {
+			        // output data of each row
+			        while($row = $result->fetch_assoc()) {
 		?>
-		             
-		          
-		    <li><a href="#"><?php echo $row["title_name"]; ?></h2></a></li>
-		  
+		
+		<form method="get" action="result.php" >
+			<button type="submit" name="title" value="<?php echo $row["title_ID"]; ?>" class="ui-btn ui-shadow ui-corner-all">
+			<?php echo $row["title_name"]; ?>
+			</button>
+		</form>
+	</a>
 
-		<?php
-		        }
-		        } else {
-		            echo "0 results";
-		        }
-		$conn->close();
+	<?php
+			        }
+			        } else {
+			            echo "0 results";
+			        }
+			$conn->close();
 
-		?>
-	</ul>
+			?>
+</div>
 
 </body>
 </html>
--- result.php
+++ result.php
@@ -1,21 +1,43 @@
-<?php
-include('db_connect.php');
-$sql = "SELECT * FROM title ORDER BY title_name ASC";
-$result = $conn->query($sql);
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anime Saitama</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width">
+<!-- CSS HERE -->
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-?>
+<!-- SCRIPT HERE -->
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
-                    
-                <?php echo $row["title_name"]; ?>
+</head>
+<body>
+	<?php
+			$title =$_GET['title'];
 
-<?php
-        }
-        } else {
-            echo "0 results";
-        }
-$conn->close();
+			include('db_connect.php');
+			$sql = "SELECT * FROM episodes WHERE episode_title_ID = '$title'";
+			$result = $conn->query($sql);
 
-?>
+			    if ($result->num_rows > 0) {
+			        // output data of each row
+			        while($row = $result->fetch_assoc()) {
+	?>
+    
+	<?php echo $row["episode_embedcode"]; ?></a>
+	
+	<?php
+			        }
+			        } else {
+			            echo "0 results";
+			        }
+			$conn->close();
+
+	?>
+
+
+
+</body>
+</html>
